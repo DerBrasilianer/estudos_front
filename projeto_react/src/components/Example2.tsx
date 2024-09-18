d4899f3511a82174477144619e2e418b946fd0c6
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 05:02:43 -0300

    Criar interface, estado e adicionar valores ao formulário

--- projeto_react/src/components/Example2.tsx
+++ projeto_react/src/components/Example2.tsx
@@ -1,6 +1,20 @@
 import { useState } from "react";
 
+interface Mydata {
+
+    name: string
+    email: string
+
+}
+
 const Example2Component = () => {
+
+    const [data, setData] = useState<Mydata>({
+
+        name: '',
+        email: ''
+
+    })
 
     return (
 
@@ -15,7 +29,7 @@ const Example2Component = () => {
                 <label htmlFor="name">
 
                     Nome <br />
-                    <input id="name" name="name" type="text" />
+                    <input id="name" name="name" type="text" value={data.name} />
 
                 </label>
 
@@ -25,7 +39,7 @@ const Example2Component = () => {
                 <label htmlFor="email">
 
                     E-mail <br />
-                    <input id="email" name="email" type="email" />
+                    <input id="email" name="email" type="email" value={data.email} />
 
                 </label>
 
