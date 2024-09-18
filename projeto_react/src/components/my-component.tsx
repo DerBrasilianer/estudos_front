e386055db3aae50547f73ef1c43e97e763499f17
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 04:23:03 -0300

    Colocar a prop children na função e substituir o texto pela variável

--- projeto_react/src/components/my-component.tsx
+++ projeto_react/src/components/my-component.tsx
@@ -6,13 +6,13 @@ interface MyComponentProps extends PropsWithChildren {
 
 }
 
-const MyComponent = ({ color }: MyComponentProps) => {
+const MyComponent = ({ color, children }: MyComponentProps) => {
 
     return (
 
         <p style={{color: color}}>
 
-            Hello, world!
+            {children}
 
         </p>
 
