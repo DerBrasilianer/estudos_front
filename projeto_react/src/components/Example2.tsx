6c2c8d95e094084adf49077343f229949004736c
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 04:51:39 -0300

    Criar arquivo Example2.tsx

---
+++ projeto_react/src/components/Example2.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Example2Component = () => {
+
+    return (
+
+        <>
+        
+            
+        
+        </>
+
+    )
+
+}
+
+export default Example2Component
