8ad6c8fc6a90d32e54357ff64c5235d6e4e9dd3f
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 04:32:22 -0300

    Adicionando a função e botão 'identifyClick'

--- projeto_react/src/components/my-component.tsx
+++ projeto_react/src/components/my-component.tsx
@@ -3,18 +3,27 @@ import { PropsWithChildren } from "react"
 interface MyComponentProps extends PropsWithChildren {
 
     color?: string
+    identifyClick?: () => void
 
 }
 
-const MyComponent = ({ color = 'blue', children }: MyComponentProps) => {
+const MyComponent = ({ color = 'blue', identifyClick, children }: MyComponentProps) => {
 
     return (
+
+        <>
 
         <p style={{color: color}}>
 
             {children}
 
         </p>
+
+        <button onClick={() => identifyClick ? identifyClick(): null}>
+            Click
+        </button>
+
+        </>
 
     )
 
