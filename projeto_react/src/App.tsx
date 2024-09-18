6e2b5a3781c80c8f83b2a464f88e85930c7a797d
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 05:32:38 -0300

    Refazer App do Vite + React com Counter

--- projeto_react/src/App.tsx
+++ projeto_react/src/App.tsx
@@ -1,13 +1,34 @@
+import { useState } from 'react'
 import './App.css'
-import Example2Component from './components/Example2'
+import Counter from './components/Counter'
 
 function App() {
+
+  const [count, setCount] = useState(0)
 
   return (
     
     <>
       
-      <Example2Component />
+      <div>
+        
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src='../public/vite.svg' className='logo' alt='Vite logo' />
+        </a>
+
+        <a href='https://react.dev' target='_blank'>
+          <img src='./src/assets/react.svg' className='logo react' alt='React logo' />
+        </a>
+
+      </div>
+
+      <h1>Vite + React</h1>
+
+      <Counter count={count} setCount={setCount} />
+
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
 
     </>
 
