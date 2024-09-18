988a8f1f2e3a7e1e2c05b4046e6c2aebea1b14c6
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 04:42:32 -0300

    Adicionar arquivo Example.tsx

---
+++ projeto_react/src/components/Example.tsx
@@ -0,0 +1,30 @@
+const ExampleComponent = () => {
+
+    return (
+
+        <>
+        
+            <h6>
+                Exemplo de estado
+            </h6>
+
+            <p>
+                Nome:
+
+                <span>
+                    Nome aqui
+                </span>
+
+            </p>
+
+            <button>
+                Clique Aqui
+            </button>
+        
+        </>
+
+    )
+
+}
+
+export default ExampleComponent
