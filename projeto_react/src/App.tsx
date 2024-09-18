50f61159b408ccf33297d021c02c7a0899a0dc40
Enzo Soddano <enzosoddano1203@gmail.com>
2024-09-18 05:38:22 -0300

    Remover imports desnecessários

--- projeto_react/src/App.tsx
+++ projeto_react/src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import Counter from './components/Counter'
 import Example3Component from './components/Example3'
 
 function App() {
-
-  const [count, setCount] = useState(0)
 
   return (
     
